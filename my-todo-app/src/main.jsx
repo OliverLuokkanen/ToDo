f96091e7ff98bcc880b0344dbb455281d80b5b9f
OliverLuokkanen <olluokka24@students.oamk.fi>
2025-12-18 01:49:28 +0200

    valmis päivitetty

--- my-todo-app/src/main.jsx
+++ my-todo-app/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./screens/App.jsx";
+import { UserProvider } from "./context/UserProvider.jsx";
+import "./index.css"; // jos sinulla on globaali tyylitiedosto, muuten tämän voi poistaa
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProvider>
   </React.StrictMode>
 );
